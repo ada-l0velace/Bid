0ff3c5ae5c2b98ccd8c9520313d082aa8eac2083
lolstorm92 <ricardo.s.campos@ist.utl.pt>
2015-10-27 05:29:01 +0000

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -3,8 +3,8 @@
 		ini_set('display_errors', 1);
 		// Variáveis de conexão à BD
 		$host="db.ist.utl.pt"; // o MySQL esta disponivel nesta maquina
-		$user="ist176563"; // -> substituir pelo nome de utilizador
-		$password="apjd9878"; // -> substituir pela password dada pelo mysql_reset
+		$user="*"; // -> substituir pelo nome de utilizador
+		$password="*"; // -> substituir pela password dada pelo mysql_reset
 		$dbname = $user; // a BD tem nome identico ao utilizador
 		#echo("<p>Projeto Base de Dados Parte II</p>\n");
 		$connection = new PDO("mysql:host=" . $host. ";dbname=" . $dbname, $user, $password, 
